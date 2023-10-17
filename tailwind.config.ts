53ee57b1dff41526578772a5e830ef0fa74a5f2d
Jason Savelli <jason.savelli@mention.com>
2023-10-17 21:35:38 +0200

    task: add design system values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      mobile: "375px",
+      desktop: "1440px",
     },
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      grey100: "hsl(0, 0%, 98%)",
+      grey500: "#C4C4C4",
+      grey700: "#B2B2B2",
+      slate500: "#2B3844",
+      slate700: "#202C36",
+    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
