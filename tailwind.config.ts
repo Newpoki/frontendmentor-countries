3842fb19c6a64a8c5c02a269cd0954fdd254602c
Jason Savelli <jason.savelli@mention.com>
2023-10-17 21:59:57 +0200

    task: add font weight to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    screens: {
-      mobile: "375px",
-      desktop: "1440px",
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        screens: {
+            mobile: '375px',
+            desktop: '1440px',
+        },
+        colors: {
+            white: '#FFFFFF',
+            black: '#000000',
+            grey100: 'hsl(0, 0%, 98%)',
+            grey500: '#C4C4C4',
+            grey700: '#B2B2B2',
+            slate500: '#2B3844',
+            slate700: '#202C36',
+        },
+        fontWeight: {
+            light: '300',
+            semibold: '600',
+            'extra-bold': '800',
+        },
+        extend: {},
     },
-    colors: {
-      white: "#FFFFFF",
-      black: "#000000",
-      grey100: "hsl(0, 0%, 98%)",
-      grey500: "#C4C4C4",
-      grey700: "#B2B2B2",
-      slate500: "#2B3844",
-      slate700: "#202C36",
-    },
-    extend: {}
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
