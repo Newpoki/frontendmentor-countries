c556fa69953a64cbfb23a050c2123d896b690a9a
Jason Savelli <jason.savelli@mention.com>
2023-10-18 01:15:40 +0200

    feat: home page layout responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Filters } from './filters/filters';
 
 export default function Home() {
     return (
-        <main className="px-4 py-6">
+        <main className="px-4 py-6 desktop:px-20 desktop:py-12">
             <Filters />
         </main>
     );
