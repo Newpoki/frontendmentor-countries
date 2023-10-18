4fa29c4083f1860734d6a1eba24c0f2612d53192
Jason Savelli <jason.savelli@mention.com>
2023-10-18 22:44:38 +0200

    fix: set viewport through meta object instead of meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,11 @@ const nunito = Nunito({ subsets: ['latin'] });
 export const metadata: Metadata = {
     title: 'Where in the world?',
     description: 'Learn more about countries !',
+    viewport: {
+        initialScale: 1,
+        maximumScale: 1,
+        width: 'device-width',
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -18,7 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             // https://github.com/pacocoursey/next-themes#with-app
             suppressHydrationWarning
         >
-            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
             <body
                 className={`${nunito.className} flex h-[100dvh] flex-col overflow-hidden bg-grey100 text-black  dark:bg-slate700 dark:text-white`}
             >
