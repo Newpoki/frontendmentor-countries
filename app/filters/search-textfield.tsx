3eb4b8d081b3ecc65ce63b151f35561ede348b4a
Jason Savelli <jason.savelli@mention.com>
2023-10-18 14:46:52 +0200

    task: search field cursor

--- app/filters/search-textfield.tsx
+++ app/filters/search-textfield.tsx
@@ -39,7 +39,7 @@ export const SearchTextField = ({ className }: Props) => {
     return (
         <label
             className={twMerge(
-                'flex items-center gap-[26px] rounded-[5px] bg-white px-8 py-[14px] shadow-md dark:bg-slate500  desktop:gap-[24px] desktop:py-[19px]',
+                'flex cursor-text items-center gap-[26px] rounded-[5px] bg-white px-8 py-[14px] shadow-md  dark:bg-slate500 desktop:gap-[24px] desktop:py-[19px]',
                 className
             )}
         >
