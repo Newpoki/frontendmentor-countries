5178ee357f3a5ca51b790f291a5d071b9934f9a4
Jason Savelli <jason.savelli@mention.com>
2023-10-18 21:48:19 +0200

    feat: header title is now a link to the homepage

--- app/header.tsx
+++ app/header.tsx
@@ -2,6 +2,7 @@
 
 import QuarterMoon from '@/public/assets/quarter-moon.svg';
 import { useTheme } from 'next-themes';
+import Link from 'next/link';
 import { useCallback } from 'react';
 
 export const Header = () => {
@@ -13,9 +14,11 @@ export const Header = () => {
 
     return (
         <header className="flex items-center justify-between bg-white px-4 py-[30px] shadow-sm dark:bg-slate500">
-            <h1 className="tablet:text-[20px] tablet:leading-none text-[14px] font-extra-bold leading-[20px]">
-                Where in the world?
-            </h1>
+            <Link href="/">
+                <h1 className="tablet:text-[20px] tablet:leading-none text-[14px] font-extra-bold leading-[20px]">
+                    Where in the world?
+                </h1>
+            </Link>
             <button className="flex items-center gap-2" onClick={handleToggleTheme}>
                 <QuarterMoon className="tablet:h-5 tablet:w-5 h-4 w-4 text-black dark:text-white" />
                 <span className="tablet:text-[16px] text-[12px] font-semibold leading-none">
