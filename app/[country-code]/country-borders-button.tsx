87c629135de66dae51c516a62e8963ec21fa2f0c
Jason Savelli <jason.savelli@mention.com>
2023-10-18 22:29:49 +0200

    fix: padding on borders button and link

--- app/[country-code]/country-borders-button.tsx
+++ app/[country-code]/country-borders-button.tsx
@@ -10,10 +10,10 @@ export const CountryBordersButton = async ({ borderCountryCode }: Props) => {
     const countryBorderCountryData = await fetchCountryData(borderCountryCode);
 
     return (
-        <CountryButton className="shadow-[0px_0px_4px_1px_rgba(0,0,0,0.10)]">
-            <Link href={`/${countryBorderCountryData.cca3.toLocaleLowerCase()}`}>
+        <Link href={`/${countryBorderCountryData.cca3.toLocaleLowerCase()}`}>
+            <CountryButton className="shadow-[0px_0px_4px_1px_rgba(0,0,0,0.10)]">
                 {countryBorderCountryData.name.common}
-            </Link>
-        </CountryButton>
+            </CountryButton>
+        </Link>
     );
 };
