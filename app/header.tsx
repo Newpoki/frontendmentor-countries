44f9448843e1eea6dd22f1a84b79a44aaec72373
Jason Savelli <jason.savelli@mention.com>
2023-10-18 23:29:47 +0200

    fix: padding on responsive header

--- app/header.tsx
+++ app/header.tsx
@@ -13,15 +13,15 @@ export const Header = () => {
     }, [setTheme, resolvedTheme]);
 
     return (
-        <header className="flex items-center justify-between bg-white px-4 py-[30px] shadow-sm dark:bg-slate500">
+        <header className="flex items-center justify-between bg-white px-4 py-[30px] shadow-sm dark:bg-slate500 desktop:px-20 desktop:py-[26px] ">
             <Link href="/">
-                <h1 className="tablet:text-[20px] tablet:leading-none text-[14px] font-extra-bold leading-[20px]">
+                <h1 className="text-[14px] font-extra-bold leading-[20px] tablet:text-[24px] tablet:leading-none">
                     Where in the world?
                 </h1>
             </Link>
             <button className="flex items-center gap-2" onClick={handleToggleTheme}>
-                <QuarterMoon className="tablet:h-5 tablet:w-5 h-4 w-4 text-black dark:text-white" />
-                <span className="tablet:text-[16px] text-[12px] font-semibold leading-none">
+                <QuarterMoon className="h-4 w-4 text-black dark:text-white tablet:h-5 tablet:w-5" />
+                <span className="text-[12px] font-semibold leading-none tablet:text-[16px]">
                     Dark Mode
                 </span>
             </button>
