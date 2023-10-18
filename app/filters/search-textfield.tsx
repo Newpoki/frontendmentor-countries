4e7e5bee48019fb5c9a992f099de30c1a3d98675
Jason Savelli <jason.savelli@mention.com>
2023-10-18 08:50:52 +0200

    feat: can now change search parameter

--- app/filters/search-textfield.tsx
+++ app/filters/search-textfield.tsx
@@ -1,11 +1,41 @@
+'use client';
+
 import Search from '@/public/assets/search.svg';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 import { twMerge } from 'tailwind-merge';
+import debounce from 'lodash.debounce';
 
 type Props = {
     className?: string;
 };
 
 export const SearchTextField = ({ className }: Props) => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const initialSearch = searchParams.get('search') ?? '';
+
+    const handleUpdateSearchParam = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const params = new URLSearchParams(searchParams);
+
+            const { value } = event.target;
+
+            if (value === '') {
+                params.delete('search');
+            } else {
+                params.set('search', event.target.value);
+            }
+
+            router.push(`${pathname}?${params}`);
+        },
+        [pathname, router, searchParams]
+    );
+
+    const debounceUpdateSearchParam = debounce(handleUpdateSearchParam, 300);
+
     return (
         <label
             className={twMerge(
@@ -17,6 +47,8 @@ export const SearchTextField = ({ className }: Props) => {
             <input
                 placeholder="Search for a country"
                 className="dark:placeholder-text-white bg-transparent text-[12px] font-[400] leading-[20px] outline-none placeholder:text-grey500 dark:text-white desktop:text-[14px]"
+                onChange={debounceUpdateSearchParam}
+                defaultValue={initialSearch}
             />
         </label>
     );
